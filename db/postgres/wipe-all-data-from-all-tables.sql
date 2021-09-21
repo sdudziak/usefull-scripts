5e084399c7955bf09a13549f42c77575075d28c5
Szymon Dudziak <sdudziak@users.noreply.github.com>
2021-09-21 09:42:34 +0200

    Create wipe-all-data-from-all-tables.sql

---
+++ db/postgres/wipe-all-data-from-all-tables.sql
@@ -0,0 +1,13 @@
+do
+$$
+declare
+  l_stmt text;
+begin
+  select 'truncate ' || string_agg(format('%I.%I', schemaname, tablename), ',')
+    into l_stmt
+  from pg_tables
+  where schemaname in ('public'); # change the 'public' do your schema name
+
+  execute l_stmt;
+end;
+$$
